655a496b93d5d6a592bc777bf32c9091cc4209c4
README.md <xxxxxx@gmail.com>
2023-02-16 07:02:26 +0000

    4.3 hero layout

--- components/hero.js
+++ components/hero.js
@@ -2,9 +2,11 @@ import styles from 'styles/hero.module.css'
 
 export default function Hero ({ title, subtitle, imageOn = false }) {
   return (
-    <div className={styles.text}>
-      <h1 className={styles.title}>CUBE</h1>
-      <p className={styles.subtitle}>アウトプットしていくサイト</p>
+    <div className={styles.flexContainer}>
+      <div className={styles.text}>
+        <h1 className={styles.title}>CUBE</h1>
+        <p className={styles.subtitle}>アウトプットしていくサイト</p>
+      </div>
       {imageOn && <figure> [画像] </figure>}
     </div>
   )
