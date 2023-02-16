fcb7b6500e3f6983f2da24b7ea8d8be8f8db4c67
README.md <xxxxxx@gmail.com>
2023-02-16 05:42:02 +0000

    2.9 propsから値を渡す

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,9 @@
-export default function Hero () {
+export default function Hero ({ title, subtitle, imageOn = false }) {
   return (
     <div>
       <h1>CUBE</h1>
       <p>アウトプットしていくサイト</p>
+      {imageOn && <figure> [画像] </figure>}
     </div>
   )
 }
