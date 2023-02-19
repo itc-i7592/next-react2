4979e46140850051b7d968e5c380ab9a3dd4f5b1
README.md <xxxxxx@gmail.com>
2023-02-19 06:05:35 +0000

    6.4 meta component

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,11 @@
 import Container from 'components/container'
+import Meta from 'components/meta'
 import Hero from 'components/hero'
 
 export default function Blog () {
   return (
     <Container>
+      <Meta pageTitle='ブログ' />
       <Hero title='Blog' subtitle='Reacts Posts' />
     </Container>
   )
